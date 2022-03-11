f9a10ab64afdb3035c5bb020268a87091099586d
gotik29 <gotik929@gmail.com>
2022-03-11 13:47:42 +0300

    testing

---
+++ js/app.js
@@ -0,0 +1,36 @@
+(() => {
+    "use strict";
+    function isWebp() {
+        function testWebP(callback) {
+            let webP = new Image;
+            webP.onload = webP.onerror = function() {
+                callback(2 == webP.height);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        testWebP((function(support) {
+            let className = true === support ? "webp" : "no-webp";
+            document.documentElement.classList.add(className);
+        }));
+    }
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    const iconMenu = document.querySelector(".menu__icon");
+    if (iconMenu) {
+        const menuBody = document.querySelector(".menu__body");
+        iconMenu.addEventListener("click", (function(e) {
+            document.body.classList.toggle("_lock");
+            iconMenu.classList.toggle("_active");
+            menuBody.classList.toggle("_active");
+        }));
+    }
+    window["FLS"] = true;
+    isWebp();
+})();
