04367ce86f6491afa1e5658bec3c5f23ac68588a
gotik29 <gotik929@gmail.com>
2022-03-12 17:10:08 +0300

    Added secondblock

--- js/app.js
+++ js/app.js
@@ -31,6 +31,56 @@
             menuBody.classList.toggle("_active");
         }));
     }
+    const APIURL = "https://api.github.com/users/";
+    const main = document.getElementById("card");
+    const script_form = document.getElementById("form");
+    const search = document.getElementById("search");
+    async function getUser(username) {
+        try {
+            const {data} = await axios(APIURL + username);
+            createUserCard(data);
+            getRepos(username);
+        } catch (err) {
+            if (404 == err.response.status) createErrorCard("No profile with this username");
+        }
+    }
+    async function getRepos(username) {
+        try {
+            const {data} = await axios(APIURL + username + "/repos?sort=created");
+            addReposToCard(data);
+        } catch (err) {
+            createErrorCard("Problem fetching repos");
+        }
+    }
+    function createUserCard(user) {
+        const userID = user.name || user.login;
+        const userBio = user.bio ? `<p>${user.bio}</p>` : "";
+        const cardHTML = `\n    <div class="card">\n    <div>\n      <img src="${user.avatar_url}" alt="${user.name}" class="avatar">\n    </div>\n    <div class="user-info">\n      <h2>${userID}</h2>\n      ${userBio}\n      <ul>\n        <li>${user.followers} <strong>Followers</strong></li>\n        <li>${user.following} <strong>Following</strong></li>\n        <li>${user.public_repos} <strong>Repos</strong></li>\n      </ul>\n\n      <div id="repos"></div>\n    </div>\n  </div>\n    `;
+        main.innerHTML = cardHTML;
+    }
+    function createErrorCard(msg) {
+        const cardHTML = `\n        <div class="card">\n            <h1>${msg}</h1>\n        </div>\n    `;
+        main.innerHTML = cardHTML;
+    }
+    function addReposToCard(repos) {
+        const reposEl = document.getElementById("repos");
+        repos.slice(0, 5).forEach((repo => {
+            const repoEl = document.createElement("a");
+            repoEl.classList.add("repo");
+            repoEl.href = repo.html_url;
+            repoEl.target = "_blank";
+            repoEl.innerText = repo.name;
+            reposEl.appendChild(repoEl);
+        }));
+    }
+    script_form.addEventListener("submit", (e => {
+        e.preventDefault();
+        const user = search.value;
+        if (user) {
+            getUser(user);
+            search.value = "";
+        }
+    }));
     window["FLS"] = true;
     isWebp();
 })();
