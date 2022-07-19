d269a70a7bc4479a38fd641ca0ba5daf35ac4f04
gotik29 <gotik929@gmail.com>
2022-07-19 20:46:06 +0300

    com
    Adaptive main page

--- js/app.js
+++ js/app.js
@@ -8914,6 +8914,19 @@
                 backgroundColor: "transparent"
             });
         }
+        function hasTouch() {
+            return "ontouchstart" in document.documentElement || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+        }
+        if (hasTouch()) try {
+            for (var si in document.styleSheets) {
+                var styleSheet = document.styleSheets[si];
+                if (!styleSheet.rules) continue;
+                for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+                    if (!styleSheet.rules[ri].selectorText) continue;
+                    if (styleSheet.rules[ri].selectorText.match(":hover")) styleSheet.deleteRule(ri);
+                }
+            }
+        } catch (ex) {}
         let scrollpos = window.scrollY;
         const mainblock__subtitle = document.querySelector(".inscription__subtitle");
         const mainblock__title = document.querySelector(".inscription__title");
